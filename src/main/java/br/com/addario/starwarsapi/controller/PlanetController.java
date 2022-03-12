371e8e7997cd5f99d77f0e0f14441a56d7e36152
José Addario <j.addario@gmail.com>
2022-03-12 12:01:24 -0300

    changing controller endpoint to be more restful

--- src/main/java/br/com/addario/starwarsapi/controller/PlanetController.java
+++ src/main/java/br/com/addario/starwarsapi/controller/PlanetController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@RestController(value = "/planets")
+@RestController(value = "/api/v1/starwars")
 @RequiredArgsConstructor
 public class PlanetController {
 
